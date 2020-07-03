b7c718885d06d498c99f24c5c18227fc22c49e69
LoneHippie <lonehippie.music@gmail.com>
2020-07-03 09:35:55 +0300

    second interation
    -cleaned up and scaled down navbar
    -added background blur on menu open status
    -added placeholder for user graph visual
    -added placeholders with dummy data for saved/followed stocks display
    -removed placeholder text
    -changed background color

--- script.js
+++ script.js
@@ -2,12 +2,57 @@ const menu = document.getElementById('menu');
 const btnOpen = document.getElementById('open');
 const btnClose = document.getElementById('close');
 
+const blur = document.getElementById('blur');
+
+const marketTimer = document.getElementById('market-timer');
+const localTimer = document.getElementById('local-timer');
+
+
+//side menu functionality
 if (menu.classList.contains('active')) menu.classList.remove('active');
+if (blur.classList.contains('active')) blur.classList.remove('active');
 
 btnOpen.addEventListener('click', function() {
     menu.classList.add('active');
+    blur.classList.add('active');
 });
 
 btnClose.addEventListener('click', function() {
     menu.classList.toggle('active');
-})
+    blur.classList.toggle('active');
+});
+
+
+//time definitions
+let hourMarket = new Date().getHours() - 7;
+let hourLocal = new Date().getHours();
+let min = new Date().getMinutes();
+min >= 30 ? min = 0.5 : min = 0;
+time = hourMarket + min;
+
+//initial load
+//market schedule
+time >= 9.5 && time < 16 ? marketTimer.textContent = `Market Closes in ${16 - time}hrs` : marketTimer.textContent = `Market Opens at 02:00`; 
+
+//local timezone menu greeting
+if (hourLocal >= 6 && hourLocal < 12) {
+    localTimer.textContent = 'Good Morning';
+} else if (hourLocal >= 12 && hourLocal <18) {
+    localTimer.textContent = 'Good Afternoon';
+} else {
+    localTimer.textContent = 'Good Evening';
+};
+
+//per minute update - disabled due to increasing menu open speed
+// setInterval(() => {
+//     time >= 9.5 && time < 16 ? marketTimer.textContent = `Market Closes in ${16 - time}hrs` : marketTimer.textContent = `Market Opens at 02:00`;
+
+//     if (hourLocal >= 6 && hourLocal < 12) {
+//         localTimer.textContent = 'Good Morning';
+//     } else if (hourLocal >= 12 && hourLocal <18) {
+//         localTimer.textContent = 'Good Afternoon';
+//     } else {
+//         localTimer.textContent = 'Good Evening';
+//     };
+
+// }, 60000);
